3edcf2e4359d10bdb1c86c5a6b814c7755a23868
ChacunGu <chacungu@gmail.com>
2022-04-21 16:23:26 +0000

    Fixing minor issues. Cleaned up ModelRace parameters.

--- Training/ModelsTrainer.py
+++ Training/ModelsTrainer.py
@@ -69,7 +69,8 @@ class ModelsTrainer:
     # public methods
 
     def select(self, pipelines, all_pipelines_txt, S, selection_len, score_margin,
-               training_set_params=None, n_splits=3, test_method=ttest_rel, p_value=.01, early_break=False):
+               training_set_params=None, n_splits=3, test_method=ttest_rel, p_value=.01, alpha=1., beta=1., gamma=1., # TODO give good values to alpha,beta,gamma
+               allow_early_eliminations=True, early_break=False):
         """
         Selects (and partially trains if pipelines' training can be paused and resumed) the most-promising pipelines.
 
@@ -85,12 +86,18 @@ class ModelsTrainer:
         test_method -- Significance test. Takes as input two lists of equal length and returns a tuples which 2nd value is the 
                        p-value. (default: scipy.stats.ttest_rel)
         p_value -- p_value used with the paired t-test to decide if a difference is significant or not (default: 0.01).
+        alpha -- alpha parameter in the score function. Weight of F1-Score. Must be between 0 and 1 (default: TODO).
+        beta -- beta parameter in the score function. Weight of Recall@3. Must be between 0 and 1 (default: TODO).
+        gamma -- gamma parameter in the score function. Weight of training time. Must be between 0 and 1 (default: TODO).
+        allow_early_eliminations -- True if early eliminations are allowed, False if every model should finish their cv-partial-training
+                                     even if early results show evidences of bad performance (default True).
         early_break -- True if the process can stop before all the iterations are done IF the target number of pipes has been reached,
                        False if all the iterations should be executed before returning. (default False)
 
         Return:
         List of selected ClfPipeline.
         """
+        print('ModelRace config:', S, alpha, beta, gamma, allow_early_eliminations)
         try:
             training_set_params = self.training_set.get_default_properties() if training_set_params is None else training_set_params
 
@@ -108,125 +115,129 @@ class ModelsTrainer:
             
             train_index = []
 
-            for i in tqdm(range(len(S))):
-                with Utils.catchtime('Selection iteration %i' % (i+1), verbose=True):
-                    n = (X_train.shape[0] * (S[i] - (S[i-1] if i > 0 else 0))) // 100 # number of data to add for partial training
+            with Utils.catchtime('ModelRace runtime', verbose=True): # TODO tmp print
+                for i in tqdm(range(len(S)), leave=False):
+                    with Utils.catchtime('Selection iteration %i' % (i+1), verbose=True): # TODO tmp print
+                        n = (X_train.shape[0] * (S[i] - (S[i-1] if i > 0 else 0))) // 100 # number of data to add for partial training
 
-                    # generate new pipelines from the remaining set of candidates
-                    if i > 0:
-                        max_nb_pipes_at_iter_i = get_max_nb_p_at_i(i) # init_nb_pipes // (pruning_factor**i)
-                        nb_pipes_to_generate = max(max_nb_pipes_at_iter_i - len(pipelines), len(pipelines)//10)
-                        new_pipes = ClfPipeline.generate_from_set(pipelines, all_pipelines_txt, nb_pipes_to_generate)
-                        pipelines.extend(new_pipes)
-                        print('\nGenerated %i new pipelines from the remaining candidates (max nb pipes at iter %i is %i).' % (len(new_pipes), i, max_nb_pipes_at_iter_i))
-                    
-                    # prepare the partial training set
-                    X_train_unused = X_train.loc[~X_train.index.isin(train_index)]
-                    y_train_unused = y_train.loc[~y_train.index.isin(train_index)]
-                    assert X_train_unused.index.identical(y_train_unused.index)
+                        # generate new pipelines from the remaining set of candidates
+                        if i > 0:
+                            max_nb_pipes_at_iter_i = get_max_nb_p_at_i(i) # init_nb_pipes // (pruning_factor**i)
+                            nb_pipes_to_generate = max(max_nb_pipes_at_iter_i - len(pipelines), len(pipelines)//10)
+                            new_pipes = ClfPipeline.generate_from_set(pipelines, all_pipelines_txt, nb_pipes_to_generate)
+                            pipelines.extend(new_pipes)
+                            print('\nGenerated %i new pipelines from the remaining candidates (max nb pipes at iter %i is %i).' % (len(new_pipes), i, max_nb_pipes_at_iter_i))
+                        
+                        # prepare the partial training set
+                        X_train_unused = X_train.loc[~X_train.index.isin(train_index)]
+                        y_train_unused = y_train.loc[~y_train.index.isin(train_index)]
+                        assert X_train_unused.index.identical(y_train_unused.index)
 
-                    try:
-                        train_index_new = sklearn_train_test_split(X_train_unused, stratify=y_train_unused, train_size=n)[0].index.tolist()
-                    except:
-                        train_index_new = sklearn_train_test_split(X_train_unused, train_size=n)[0].index.tolist()
-                    assert all(id not in train_index for id in train_index_new)
+                        try:
+                            train_index_new = sklearn_train_test_split(X_train_unused, stratify=y_train_unused, train_size=n)[0].index.tolist()
+                        except:
+                            train_index_new = sklearn_train_test_split(X_train_unused, train_size=n)[0].index.tolist()
+                        assert all(id not in train_index for id in train_index_new)
 
-                    train_index.extend(train_index_new)
-                    print('\n1/3 - begining of new partial training: %i%% -> %i' % (S[i], len(train_index))) # TODO tmp print
-                    
-                    Xp_train = X_train.loc[train_index]
-                    yp_train = y_train.loc[train_index]
-                    assert Xp_train.index.identical(yp_train.index)
+                        train_index.extend(train_index_new)
+                        print('\n1/3 - begining of new partial training: %i%% -> %i' % (S[i], len(train_index))) # TODO tmp print
+                        
+                        Xp_train = X_train.loc[train_index]
+                        yp_train = y_train.loc[train_index]
+                        assert Xp_train.index.identical(yp_train.index)
 
-                    # train, eval and perform statistic tests
-                    # create params list
-                    rmvd_pipes = manager.dict()
-                    param_list = []
-                    for pipe in pipelines:
-                        n_splits_ = n_splits if len(pipe.scores) > 0 else 10 + (i * n_splits)
-                        for train_index_cv, _ in StratifiedKFold(n_splits=n_splits_).split(Xp_train, yp_train):
-                            param_list.append(
-                                (pipe, train_index_cv, rmvd_pipes, # dynamic params
-                                 Xp_train, yp_train, X_val, y_val, all_data.columns, self.training_set.get_labeler_properties(), labels_set) # constant params
-                            )
-                    rdm.shuffle(param_list) # shuffle the param list such that the premature elimination is more efficient
-                    
-                    # run _parallel_training
-                    metrics = {}
-                    max_score = -np.inf
-                    with Pool() as pool:
-                        for res in tqdm(pool.imap_unordered(_parallel_training, param_list), total=len(param_list), leave=False):
-                            if res is not None:
-                                p_id, f1, r3, t = res
-                                # save the evaluation results
-                                if p_id not in metrics:
-                                    metrics[p_id] = []
-                                metrics[p_id].append((f1, r3, t))
+                        # train, eval and perform statistic tests
+                        # create params list
+                        rmvd_pipes = manager.dict()
+                        param_list = []
+                        for pipe in pipelines:
+                            n_splits_ = n_splits if len(pipe.scores) > 0 else 10 + (i * n_splits)
+                            for train_index_cv, _ in StratifiedKFold(n_splits=n_splits_).split(Xp_train, yp_train):
+                                param_list.append(
+                                    (pipe, train_index_cv, rmvd_pipes, # dynamic params
+                                    Xp_train, yp_train, X_val, y_val, all_data.columns, self.training_set.get_labeler_properties(), labels_set) # constant params
+                                )
+                        rdm.shuffle(param_list) # shuffle the param list such that the premature elimination is more efficient
+                        
+                        # run _parallel_training
+                        metrics = {}
+                        max_score = -np.inf
+                        with Pool() as pool:
+                            for res in tqdm(pool.imap_unordered(_parallel_training, param_list), total=len(param_list), leave=False):
+                                if res is not None:
+                                    p_id, f1, r3, t = res
+                                    # save the evaluation results
+                                    if p_id not in metrics:
+                                        metrics[p_id] = []
+                                    metrics[p_id].append((f1, r3, t))
 
-                                if len(metrics[p_id]) >= 5:
-                                    mean_score = np.mean([f1 for (f1,_,_) in metrics[p_id]]) # avg of f1-scores
-                                    if mean_score > max_score:
-                                        max_score = mean_score
-                                    elif mean_score < max_score - score_margin: # eliminate prematurely if the pipe performs really poorly
-                                        rmvd_pipes[p_id] = True # eliminate this pipe prematurely (do not finish its cross-validation training)
-                                        print('%i has been eliminated: avg_score=%.2f and max_score=%.2f' % (p_id, mean_score, max_score))
-                    print('\n%i pipelines\' training have been stopped prematurely due to poor performances.' % len(rmvd_pipes)) # TODO tmp print
-                    
-                    print('\n3/3 - statistic tests') # TODO tmp print
-                    # normalize runtime between 0 and 1 & compute the score of each pipeline on each cv-split
-                    metrics = {p_id: val for p_id, val in metrics.items() if p_id not in rmvd_pipes}
-                    max_runtime = max([i[2] for cv_scores in metrics.values() for i in cv_scores])
-                    scores_ = {id: [ModelsTrainer._compute_selection_score(f1,r3,t/max_runtime) for f1,r3,t in cv_scores] for id, cv_scores in metrics.items()}
-                    
-                    # add the newly measured scores to the global scores list & remove the pipes that were stopped prematurely
-                    for i in range(len(pipelines) - 1, -1, -1):
-                        pipe = pipelines[i]
-                        if pipe.id in scores_:
-                            pipe.scores.extend(scores_[pipe.id])
-                        else:
-                            del pipelines[i]
-                    
-                    # statistical tests - remove pipes that are significantly worse than any other pipe
-                    less_aggressive_pruning = (len(pipelines) <= get_max_nb_p_at_i(i)) if i > 0 else True
-                    worse_pipes = self._apply_test(
-                        pipelines, 
-                        test_method,
-                        K=pruning_factor,
-                        p_value=p_value,
-                        less_aggressive_pruning=less_aggressive_pruning
-                    )
+                                    # early eliminations
+                                    if allow_early_eliminations and len(metrics[p_id]) >= 5:
+                                        mean_score = np.mean([f1 for (f1,_,_) in metrics[p_id]]) # avg of f1-scores
+                                        if mean_score > max_score:
+                                            max_score = mean_score
+                                        elif mean_score < max_score - score_margin: # eliminate prematurely if the pipe performs really poorly
+                                            rmvd_pipes[p_id] = True # eliminate this pipe prematurely (do not finish its cross-validation training)
+                                            print('%i has been eliminated early: avg_score=%.2f and max_score=%.2f' % (p_id, mean_score, max_score))
+                        print('\n%i pipelines\' training have been stopped prematurely due to poor performances.' % len(rmvd_pipes)) # TODO tmp print
+                        
+                        print('\n3/3 - statistic tests') # TODO tmp print
+                        # normalize runtime between 0 and 1 & compute the score of each pipeline on each cv-split
+                        metrics = {p_id: val for p_id, val in metrics.items() if p_id not in rmvd_pipes}
+                        max_runtime = max([i[2] for cv_scores in metrics.values() for i in cv_scores])
+                        scores_ = {id: [ModelsTrainer._compute_selection_score(f1,r3,t/max_runtime, alpha, beta, gamma)
+                                        for f1,r3,t in cv_scores] 
+                                    for id, cv_scores in metrics.items()}
+                        
+                        # add the newly measured scores to the global scores list & remove the pipes that were stopped prematurely
+                        for i in range(len(pipelines) - 1, -1, -1):
+                            pipe = pipelines[i]
+                            if pipe.id in scores_:
+                                pipe.scores.extend(scores_[pipe.id])
+                            else:
+                                del pipelines[i]
+                        
+                        # statistical tests - remove pipes that are significantly worse than any other pipe
+                        less_aggressive_pruning = (len(pipelines) <= get_max_nb_p_at_i(i)) if i > 0 else True
+                        worse_pipes = self._apply_test(
+                            pipelines, 
+                            test_method,
+                            K=pruning_factor,
+                            p_value=p_value,
+                            less_aggressive_pruning=less_aggressive_pruning
+                        )
 
-                    if len(pipelines) < 20: # TODO tmp print
-                        print([(p.id, np.mean(p.scores)) for p in pipelines]) # TODO tmp print
-                        print([
-                            (test_method(a.scores, b.scores)[1], a.rm.id,b.rm.id)  # we keep the worse pipelines of the 2
-                                for a,b in itertools.combinations(pipelines, r=2) # for all pairs of pipes
-                        ]) # TODO tmp print
+                        if len(pipelines) < 20: # TODO tmp print
+                            print([(p.id, np.mean(p.scores)) for p in pipelines]) # TODO tmp print
+                            print([
+                                (test_method(a.scores, b.scores)[1], a.rm.id,b.rm.id)  # we keep the worse pipelines of the 2
+                                    for a,b in itertools.combinations(pipelines, r=2) # for all pairs of pipes
+                            ]) # TODO tmp print
 
-                    # remove the worse pipes
-                    pipelines = [p for p in pipelines if p not in worse_pipes]
+                        # remove the worse pipes
+                        pipelines = [p for p in pipelines if p not in worse_pipes]
 
-                    print('\nThere remains %i pipelines. %i have been eliminated by t-test.' % (len(pipelines), len(worse_pipes)))
+                        print('\nThere remains %i pipelines. %i have been eliminated by t-test.' % (len(pipelines), len(worse_pipes)))
 
-                    if early_break and len(pipelines) <= selection_len:
-                        break
-
-            # if we have more pipes remaining that what we wanted
-            if len(pipelines) > selection_len: 
-                print('Too many pipelines remaining. Last attempt to eliminate "worse" candidates.')
-                pipelines = sorted(pipelines, key=lambda p: np.mean(p.scores), reverse=True)
-                for i in reversed(range(len(pipelines))): # rank pipes based on their avg scores
-                    p_i = pipelines[i]
-
-                    # pairwise ttest if p is worse than any other: prune p
-                    for j, p_j in enumerate(pipelines):
-                        if i is not j and test_method(p_i.scores, p_j.scores)[1] < p_value and np.mean(p_i.scores) < np.mean(p_j.scores):
-                            print('%i was eliminated due to significantly worse performances than another candidate.' % p_i.id)
-                            del pipelines[i]
+                        if early_break and len(pipelines) <= selection_len:
                             break
-                            
-                    if len(pipelines) <= selection_len:
-                        break
+
+                # if we have more pipes remaining that what we wanted
+                if len(pipelines) > selection_len: 
+                    print('Too many pipelines remaining. Last attempt to eliminate "worse" candidates.')
+                    pipelines = sorted(pipelines, key=lambda p: np.mean(p.scores), reverse=True)
+                    for i in reversed(range(len(pipelines))): # rank pipes based on their avg scores
+                        p_i = pipelines[i]
+
+                        # pairwise ttest if p is worse than any other: prune p
+                        for j, p_j in enumerate(pipelines):
+                            if i is not j and test_method(p_i.scores, p_j.scores)[1] < p_value and np.mean(p_i.scores) < np.mean(p_j.scores):
+                                print('%i was eliminated due to significantly worse performances than another candidate.' % p_i.id)
+                                del pipelines[i]
+                                break
+                                
+                        if len(pipelines) <= selection_len:
+                            break
 
         except Exception:
             import logging
@@ -333,7 +344,7 @@ class ModelsTrainer:
                                                           all_data.columns, self.training_set.get_labeler_properties(), labels_set,
                                                           plot_cm=True, save_if_best=save_if_best)
 
-                    cm[0].savefig('Training/Results/Plots/%s: %i' % (model, split_id)) # TODO tmp print
+                    #cm[0].savefig('Training/Results/Plots/%s: %i' % (model, split_id)) # TODO tmp print
 
                     # save results
                     # TrainResults contain a dict grouping the results of each trained model
@@ -357,7 +368,7 @@ class ModelsTrainer:
     
     # static methods
 
-    def _compute_selection_score(f1, r3, t, alpha=1., beta=0.75, gamma=0.5):
+    def _compute_selection_score(f1, r3, t, alpha, beta, gamma):
         """
         Method that computes the score based on which a model will be evaluated during the selection process. 
 

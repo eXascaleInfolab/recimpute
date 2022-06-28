256b27958884a233f8022d134c5521c687ea4318
ZakharTymchenko <3822929+ZakharTymchenko@users.noreply.github.com>
2022-06-28 12:41:38 +0200

    added datasets download scripts, added folder structure for data

--- install_script.sh
+++ install_script.sh
@@ -8,3 +8,6 @@ python3.8 -m venv venv
 . venv/bin/activate
 python3.8 -m pip install --upgrade pip
 pip install -r requirements.txt
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Z8cU5S5v5hITrP_jD56UzqfUTPRjQrxI' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Z8cU5S5v5hITrP_jD56UzqfUTPRjQrxI" -O datasets.zip && rm -rf /tmp/cookies.txt
+unzip datasets.zip -d Datasets/
